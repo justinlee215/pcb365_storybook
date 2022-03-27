eccba26ed2b51fef7a6041f720f4c87a16492747
DevNeoLee <devneolee@gmail.com>
2022-03-26 19:45:22 -0700

    Uncomment photos on homepage

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,12 @@ import styles from '../styles/Home.module.scss'
 import Layout from '../components/layout/Layout'
 
 import logo from '../public/images/logo_black.png'
-// import trade1 from '../public/images/trade1.jpg'
-// import trade2 from '../public/images/trade2.jpg'
-// import trade3 from '../public/images/trade3.jpg'
-// import trade4 from '../public/images/trade4.jpg'
-// import trade5 from '../public/images/trade5.jpg'
-// import trade6 from '../public/images/trade6.jpg'
+import trade1 from '../public/images/trade1.jpg'
+import trade2 from '../public/images/trade2.jpg'
+import trade3 from '../public/images/trade3.jpg'
+import trade4 from '../public/images/trade4.jpg'
+import trade5 from '../public/images/trade5.jpg'
+import trade6 from '../public/images/trade6.jpg'
 
 import { Button } from 'react-bootstrap';
 
@@ -27,14 +27,14 @@ export default function Home(props) {
         <div className="nani">Hello World!</div>
         <Button variant="flat" size="xxl" className={styles.flat}>ohoho</Button>
         <div className={styles.box}>test</div>
-        {/* {[trade1, trade2, trade3, trade4, trade5, trade6].map((ele, i)=> { 
+        {[trade1, trade2, trade3, trade4, trade5, trade6].map((ele, i)=> { 
           return (
           <div className="largeBox" key={i}>
               <Image src={ele} width={500} height={500}></Image>
           </div>
         )
           }
-          )} */}
+          )}
       </main>
     </Layout>
   )
