b5aade6415242cd1df05031ba35d5cd8abbe0bfa
justinlee215 <justin.lee@pacificgroup.net>
2022-03-28 20:57:06 -0700

    Add React-Spring, implement Log in Form with animation

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 // import styles from '../styles/Home.module.scss'
 
 import Layout from '../components/layout/Layout'
@@ -13,21 +13,63 @@ import trade3 from '../public/images/trade3.jpg'
 // import trade5 from '../public/images/trade5.jpg'
 // import trade6 from '../public/images/trade6.jpg'
 
+import { useSpring, animated } from 'react-spring'
+
+
 import Card from '../components/card/Card'
 
 import { Button } from 'react-bootstrap'
 
-export default function Home(props) {
 
-    const [text, setText] = React.useState("waiting...");
-  
-    React.useEffect(() => {
-      let isCancelled = false;
-  
-      return () => {
-        isCancelled = true;
-      };
-    }, []);
+function Example() {
+  const example = useSpring({ reset: true, delay: 200, to: { opacity: 0 }, from: { opacity: 1 } })
+  return <animated.div style={example}>I will fade in</animated.div>
+}
+
+function SVG() {
+  const [flip, set] = useState(false)
+  const { x } = useSpring({
+    reset: true,
+    reverse: flip,
+    from: { x: 0 },
+    x: 1,
+    delay: 200,
+    // config: config.molasses,
+    onRest: () => set(!flip),
+  })
+
+  return (
+    <animated.svg
+      style={{ margin: 20, width: 80, height: 80 }}
+      viewBox="0 0 45 44"
+      strokeWidth="2"
+      fill="white"
+      stroke="rgb(45, 55, 71)"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeDasharray={156}
+      strokeDashoffset={x.to(x => (1 - x) * 156)}>
+      <polygon points={6} />
+    </animated.svg>
+  )
+}
+
+function Number() {
+  const [flip, set] = useState(false)
+  const { number } = useSpring({
+    reset: true,
+    reverse: flip,
+    from: { number: 0 },
+    number: 100,
+    delay: 200,
+    // config: config.molasses,
+    onRest: () => set(!flip),
+  })
+
+  return <animated.div>{number.to(n => n.toFixed(3))}</animated.div>
+}
+
+export default function Home() {
 
   return (
     <Layout>
@@ -37,6 +79,9 @@ export default function Home(props) {
         <link rel="icon" href="/favicon.png" />
       </Head>
       <main className="main">
+        <Example />
+        <Number />
+        <SVG />
         <div className="largebox">
             <Image src={trade2} width={1050} height={650} priority ></Image>
             <Card />
--- pages/logIn/index.js
+++ pages/logIn/index.js
@@ -7,6 +7,11 @@ import logo from '../../public/images/logo_color.png'
 import googleLogo from '../../public/images/google_logo.png'
 import facebookLogo from '../../public/images/facebook_logo.png'
 
+import { ArrowLeftCircle } from 'react-bootstrap-icons'
+
+import { useTransition, useSpring, animated } from 'react-spring'
+
+
 
 // import { useSession, signIn, signOut, getSession, getCsrfToken } from "next-auth/react";
 import Link from 'next/link';
@@ -15,52 +20,116 @@ import { Form, Button } from 'react-bootstrap'
 
 export default function LogIn() {
 
-  return (
-  
-    <div className={styles.container}>
-        <Head>
-    <title>Log in - PCB365</title>
-    <meta name="description" content="CSS Big Bang" />
-    <link rel="icon" href="/favicon.png" />
-  </Head>
-      <Form className={styles.form}>
-          <div className={styles.logo_container}>
-        <Link href="/">
-            <Image src={logo} prioty alt="PCB Logo" width={125} height={39} className={styles.logo}/>
-        </Link>
-            <h3>Log in</h3>
-          </div>
-        <Form.Group className="mb-12" 
-          // onChange={(e) => setUser({ ...user, username: e.target.value })}
-          controlId="formBasicEmail"
-          >
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter address" />
-          <Form.Text className="text-muted">
-          </Form.Text>
-        </Form.Group>
+  const [ forgot, setForgot ] = useState([{}])
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <a href="/" className={styles.forgot}><p>Forgot password?</p></a>
-        <a href="/" className={styles.forgot}><p>Sign Up</p></a>
+  const handleClickForgot = () => {
+    setForgot(forgot => !forgot)
+  }
+
+  const transition = useTransition( forgot, {
+    from: { x: 10, y: 0, opacity: 0},
+    enter: { x: 0, y: 0, opacity: 1}
+  })
+
+  const transition2 = useTransition( forgot, {
+    from: { x: 50, y: 0, opacity: 0},
+    enter: { x: 0, y: 0, opacity: 1}
+  })
+
+  const transition3 = useTransition( forgot, {
+    from: { x: 700, y: 0, opacity: 0},
+    enter: { x: 0, y: 0, opacity: 1},
+    leave: { x: -300, y: 0, opacity: 0}
+  })
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Log in - PCB365</title>
+        <meta name="description" content="CSS Big Bang" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <Form className={styles.form}>
+          <div className={styles.upperForm}>
+            { transition((style, item) =>
+              item ?  
+                <animated.div style={style} className={styles.upperFormBackButton}><ArrowLeftCircle onClick={ handleClickForgot } className={styles.backButton} size={20} aria-label="Back Button" /> </animated.div>
+              :
+                ''
+            )}
+            <Link href="/">
+                <Image src={logo} alt="PCB Logo" width={125} height={39} className={styles.logo}/>
+            </Link>
+
+            { transition2((style, item) => 
+            item ?  
+              <animated.h3 style={style}>Reset your password</animated.h3>
+              :
+              ""
+            )}
+
+            { transition2((style, item) => 
+            item ?  
+              ""
+              :
+              <animated.h3 style={style}>Log in</animated.h3>
+            )}
+
+          </div>
+
+          { transition3((style, item) => 
+          item ?  
+          <animated.div >
+            <p>Please enter your email address. We will send you an email to reset your password.</p>
+          </animated.div>
+            :
+                ""
+            )}
+            <Form.Group className="mb-12" 
+              // onChange={(e) => setUser({ ...user, username: e.target.value })}
+              controlId="formBasicEmail"
+              >
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter address" />
+              <Form.Text className="text-muted">
+              </Form.Text>
+            </Form.Group>
+
+          { transition3((style, item) =>
+          item ?
+            ""
+            :
+            <animated.div style={style}>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+              <a className={styles.forgotButton} onClick={ handleClickForgot }><p>Forgot password?</p></a>
+              <a href="/" className={styles.signupButton}><p>Sign Up</p></a>
+            </animated.div>
+          )}
 
         {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
           <Form.Check type="checkbox" label="Check me out" />
         </Form.Group> */}
-
-        <Button variant="primary" type="submit" >
-          <div>Continue</div>
-        </Button>
-        <div className={styles.or}>or </div>
-        <Button type="submit" style={{ display: "flex", alignItems: "center", justifyContent: "center", textAlign: "center", backgroundColor: "white", border: "1px solid #002c71", color: "#002c71" }}>
-          <div className="buttonLogo">Continue with<Image src={googleLogo} alt="Google Logo" width={42} height={42} className={styles.logo}/></div>
-        </Button>
-        <Button type="submit" style={{ display: "flex", alignItems: "center", justifyContent: "center", textAlign: "center", backgroundColor: "white", border: "1px solid #002c71", color: "#002c71" }}>
-          <div className="buttonLogo2">Continue with<Image src={facebookLogo} alt="Facebook Logo" width={25} height={25} className={styles.logoFacebook}/></div>
-        </Button>
+            { forgot ?  
+              <Button variant="primary" type="submit" >
+                <div>Send Email</div>
+              </Button>
+              :
+              <>
+                <Button variant="primary" type="submit" >
+                  <div>Continue</div>
+                </Button>
+                <div className={styles.or}>or </div>
+                <Button type="submit" style={{ display: "flex", alignItems: "center", justifyContent: "center", textAlign: "center", backgroundColor: "white", border: "1px solid #002c71", color: "#002c71" }}>
+                  <div className="buttonLogo">Continue with<Image src={googleLogo} alt="Google Logo" width={42} height={42} className={styles.logo}/></div>
+                </Button>
+                <Button type="submit" style={{ display: "flex", alignItems: "center", justifyContent: "center", textAlign: "center", backgroundColor: "white", border: "1px solid #002c71", color: "#002c71" }}>
+                  <div className="buttonLogo2">Continue with<Image src={facebookLogo} alt="Facebook Logo" width={25} height={25} className={styles.logoFacebook}/></div>
+                </Button>
+              </>
+            }
     </Form>
   </div>
 
