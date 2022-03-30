4af1c14befc6f513b01e7d1c773d3d7748b6a845
DevNeoLee <devneolee@gmail.com>
2022-03-29 21:43:16 -0700

    Add Sign up and Add Footer Logo Image

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -1,57 +1,58 @@
 import React from "react"
 import Image from "next/image"
-import logo from '../../public/images/logo_white_1.png'
+import logo from '../../public/images/logo_color.png'
+import favicon from '../../public/favicon.png'
 
 export default function Footer() {
   return (
     <>
-      <div class="container" id="footer">
-        <footer class="pt-4 my-md-5 pt-md-5 border-top">
-          <div class="row">
-            <div class="col-12 col-md">
-              <a class="navbar-brand" href="/">
+      <div className="container" id="footer">
+        <footer className="pt-4 my-md-5 pt-md-5 border-top">
+          <div className="row">
+            <div className="col-12 col-md">
+              <a className="navbar-brand" href="/">
                 {" "}
                 <Image
-                  src="/PCB-logo-1800px-p-500.png"
+                  src={logo}
                   alt="PCB Logo"
                   width={250}
                   height={78}
                 />
               </a>
-              <small class="d-block mb-3 text-muted">&copy; 2022</small>
+              <small className="d-block mb-3 text-muted">&copy; 2022</small>
             </div>
-            <div class="col-6 col-md">
-              <h5 class="mb-2">Features</h5>
-              <ul class="list-unstyled text-small">
-                <li class="mb-1">Log in </li>
-                <li class="mb-1">Sign up </li>
-                <li class="mb-1 text-muted">eForms</li>
-                <li class="mb-1 text-muted">Shipment</li>
-                <li class="mb-1">Reports </li>
-                <li class="mb-1">Documentation </li>
-                <li class="mb-1">Your eForms List</li>
-                <li class="mb-1">Your Documentation</li>
-                <li class="mb-1">Your Cart</li>
+            <div className="col-6 col-md">
+              <h5 className="mb-2">Features</h5>
+              <ul className="list-unstyled text-small">
+                <li className="mb-1">Log in </li>
+                <li className="mb-1">Sign up </li>
+                <li className="mb-1 text-muted">eForms</li>
+                <li className="mb-1 text-muted">Shipment</li>
+                <li className="mb-1">Reports </li>
+                <li className="mb-1">Documentation </li>
+                <li className="mb-1">Your eForms List</li>
+                <li className="mb-1">Your Documentation</li>
+                <li className="mb-1">Your Cart</li>
               </ul>
             </div>
-            <div class="col-6 col-md">
-              <h5 class="mb-2">Resources</h5>
-              <ul class="list-unstyled text-small">
-                <li class="mb-2">
+            <div className="col-6 col-md">
+              <h5 className="mb-2">Resources</h5>
+              <ul className="list-unstyled text-small">
+                <li className="mb-2">
                   Photos from{" "}
                   <a
-                    class="text-muted"
+                    className="text-muted"
                     href="https://www.unsplash.com/"
                     target="_blank"
                   >
                     Unsplash
                   </a>
                 </li>
-                <li class="mb-2">Photo Artists</li>
-                <li class="mb-2">
+                <li className="mb-2">Photo Artists</li>
+                <li className="mb-2">
                   People vector Created by{" "}
                   <a
-                    class="text-muted"
+                    className="text-muted"
                     href=""
                     target="_blank"
                   >
@@ -59,10 +60,10 @@ export default function Footer() {
                     Freepik
                   </a>
                 </li>
-                <li class="mb-2">
+                <li className="mb-2">
                   {" "}
                   <a
-                    class="text-muted"
+                    className="text-muted"
                     href=""
                     target="_blank"
                   >
@@ -72,38 +73,32 @@ export default function Footer() {
                 </li>
               </ul>
             </div>
-            <div class="col-6 col-md">
-              <h5 class="mb-2">About Us</h5>
-              <ul class="list-unstyled text-small">
+            <div className="col-6 col-md">
+              <h5 className="mb-2">About Us</h5>
+              <ul className="list-unstyled text-small">
                 <li>
-                  <div class="text-muted mt-2 mb-4 d-flex">
+                  <div className="text-muted mt-2 mb-4 d-flex">
                     <a
                       href=""
                       target="_blank"
                     >
-                      <span class="mr-1">© PCB365 </span>
-                      <Image
-                        src="/PCB-logo-1800px-p-500.png"
-                        alt="PCB Logo"
-                        width={250}
-                        height={78}
-                      />{" "}
+                      <span className="mr-1">© PCB365 </span>
                     </a>
                   </div>
                 </li>
-                <li class="mb-4">
+                <li className="mb-4">
                   <a target="_blank" href="">
-                    <span class="mr-2">PCB</span>
-                    <i class="fab fa-github fa-lg"></i>
+                    <span className="mr-2">PCB</span>
+                    <i className="g"></i>
                   </a>
                 </li>
-                <li class="mt-2 mb-4">
+                <li className="mt-2 mb-4">
                   <a
                     target="_blank"
                     href=""
                   >
-                    <span class="mr-2">PCBUSA</span>
-                    <i class="fab fa-linkedin fa-lg"></i>
+                    <span className="mr-2">PCBUSA</span>
+                    <i className=""></i>
                   </a>
                 </li>
               </ul>
--- pages/logIn/index.js
+++ pages/logIn/index.js
@@ -16,10 +16,15 @@ import { Form, Button } from 'react-bootstrap'
 
 export default function LogIn() {
 
-  const [ forgot, setForgot ] = useState([{}])
+  const [ forgot, setForgot ] = useState(false)
+  const [ firstTime, setFirstTime ] = useState(false)
 
   const handleClickForgot = () => {
     setForgot(forgot => !forgot)
+  }
+
+  const handleClickFirstTime = () => {
+    setFirstTime(firstTime => !firstTime)
   }
 
   const transition = useTransition( forgot, {
@@ -38,6 +43,12 @@ export default function LogIn() {
     leave: { x: -300, y: 0, opacity: 0}
   })
 
+  const transition4 = useTransition(firstTime, {
+    from: { x: 700, y: 0, opacity: 0 },
+    enter: { x: 0, y: 0, opacity: 1 },
+    leave: { x: -300, y: 0, opacity: 0 }
+  })
+
   return (
     <div className={styles.container}>
       <Head>
@@ -45,44 +56,18 @@ export default function LogIn() {
         <meta name="description" content="CSS Big Bang" />
         <link rel="icon" href="/favicon.png" />
       </Head>
+      {
+      firstTime ?
       <Form className={styles.form}>
           <div className={styles.upperForm}>
-            { transition((style, item) =>
-              item ?  
-                <animated.div style={style} className={styles.upperFormBackButton}><ArrowLeftCircle onClick={ handleClickForgot } className={styles.backButton} size={20} aria-label="Back Button" /> </animated.div>
-              :
-                ''
-            )}
+            <div className={styles.upperFormBackButton}><ArrowLeftCircle onClick={ handleClickFirstTime } className={styles.backButton} size={20} aria-label="Back Button" /> </div>
             <Link href="/">
                 <Image src={logo} alt="PCB Logo" width={125} height={39} className={styles.logo}/>
             </Link>
-
-            { transition2((style, item) => 
-            item ?  
-              <animated.h3 style={style}>Reset your password</animated.h3>
-              :
-              ""
-            )}
-
-            { transition2((style, item) => 
-            item ?  
-              ""
-              :
-              <animated.h3 style={style}>Log in</animated.h3>
-            )}
-
+              <h3>Sign up</h3>
           </div>
-
-          { transition3((style, item) => 
-          item ?  
-          <animated.div >
-            <p>Please enter your email address. We will send you an email to reset your password.</p>
-          </animated.div>
-            :
-                ""
-            )}
+            <p>Please enter your infomation to sign up.</p>
             <Form.Group className="mb-12" 
-              // onChange={(e) => setUser({ ...user, username: e.target.value })}
               controlId="formBasicEmail"
               >
               <Form.Label>Email address</Form.Label>
@@ -90,8 +75,74 @@ export default function LogIn() {
               <Form.Text className="text-muted">
               </Form.Text>
             </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control type="password" placeholder="Confirm Password" />
+            </Form.Group>
+            <a className={styles.signupButton} onClick={ handleClickFirstTime}><p>Log in</p></a>
+            <Button variant="primary" type="submit" >
+              <div>Continue</div>
+            </Button>
+            <div className={styles.or}>or </div>
+            <Button type="submit" style={{ display: "flex", alignItems: "center", justifyContent: "center", textAlign: "center", backgroundColor: "white", border: "1px solid #002c71", color: "#002c71" }}>
+              <div className="buttonLogo">Continue with<Image src={googleLogo} alt="Google Logo" width={42} height={42} className={styles.logo}/></div>
+            </Button>
+            <Button type="submit" style={{ display: "flex", alignItems: "center", justifyContent: "center", textAlign: "center", backgroundColor: "white", border: "1px solid #002c71", color: "#002c71" }}>
+              <div className="buttonLogo2">Continue with<Image src={facebookLogo} alt="Facebook Logo" width={25} height={25} className={styles.logoFacebook}/></div>
+            </Button>
+        </Form>
+        :
+      <Form className={styles.form}>
+        <div className={styles.upperForm}>
+          {transition((style, item) =>
+            item ?
+              <animated.div style={style} className={styles.upperFormBackButton}><ArrowLeftCircle onClick={handleClickForgot} className={styles.backButton} size={20} aria-label="Back Button" /> </animated.div>
+              :
+              ''
+          )}
+          <Link href="/">
+                <Image src={logo} alt="PCB Logo" width={125} height={39} className={styles.logo}/>
+            </Link>
 
-          { transition3((style, item) =>
+          {transition2((style, item) =>
+            item ?
+              <animated.h3 style={style}>Reset your password</animated.h3>
+              :
+              ""
+          )}
+
+          {transition2((style, item) =>
+            item ?
+              ""
+              :
+              <animated.h3 style={style}>Log in</animated.h3>
+          )}
+
+        </div>
+
+        {transition3((style, item) =>
+          item ?
+            <animated.div >
+              <p>Please enter your email address. We will send you an email to reset your password.</p>
+            </animated.div>
+            :
+            ""
+        )}
+        <Form.Group className="mb-12"
+          // onChange={(e) => setUser({ ...user, username: e.target.value })}
+          controlId="formBasicEmail"
+        >
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter address" />
+          <Form.Text className="text-muted">
+          </Form.Text>
+        </Form.Group>
+
+        {transition3((style, item) =>
           item ?
             ""
             :
@@ -100,33 +151,35 @@ export default function LogIn() {
                 <Form.Label>Password</Form.Label>
                 <Form.Control type="password" placeholder="Password" />
               </Form.Group>
-              <a className={styles.forgotButton} onClick={ handleClickForgot }><p>Forgot password?</p></a>
-              <a href="/" className={styles.signupButton}><p>Sign Up</p></a>
+              <a className={styles.forgotButton} onClick={handleClickForgot}><p>Forgot password?</p></a>
+              <a className={styles.signupButton} onClick ={handleClickFirstTime}><p>Sign Up</p></a>
             </animated.div>
-          )}
+        )}
 
         {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
           <Form.Check type="checkbox" label="Check me out" />
         </Form.Group> */}
-            { forgot ?  
-              <Button variant="primary" type="submit" >
-                <div>Send Email</div>
-              </Button>
-              :
-              <>
-                <Button variant="primary" type="submit" >
-                  <div>Continue</div>
-                </Button>
-                <div className={styles.or}>or </div>
-                <Button type="submit" style={{ display: "flex", alignItems: "center", justifyContent: "center", textAlign: "center", backgroundColor: "white", border: "1px solid #002c71", color: "#002c71" }}>
-                  <div className="buttonLogo">Continue with<Image src={googleLogo} alt="Google Logo" width={42} height={42} className={styles.logo}/></div>
-                </Button>
-                <Button type="submit" style={{ display: "flex", alignItems: "center", justifyContent: "center", textAlign: "center", backgroundColor: "white", border: "1px solid #002c71", color: "#002c71" }}>
-                  <div className="buttonLogo2">Continue with<Image src={facebookLogo} alt="Facebook Logo" width={25} height={25} className={styles.logoFacebook}/></div>
-                </Button>
-              </>
-            }
-    </Form>
-  </div>
+        {forgot ?
+          <Button variant="primary" type="submit" >
+            <div>Send Email</div>
+          </Button>
+          :
+          <>
+            <Button variant="primary" type="submit" >
+              <div>Continue</div>
+            </Button>
+            <div className={styles.or}>or </div>
+            <Button type="submit" style={{ display: "flex", alignItems: "center", justifyContent: "center", textAlign: "center", backgroundColor: "white", border: "1px solid #002c71", color: "#002c71" }}>
+              <div className="buttonLogo">Continue with<Image src={googleLogo} alt="Google Logo" width={42} height={42} className={styles.logo} /></div>
+            </Button>
+            <Button type="submit" style={{ display: "flex", alignItems: "center", justifyContent: "center", textAlign: "center", backgroundColor: "white", border: "1px solid #002c71", color: "#002c71" }}>
+              <div className="buttonLogo2">Continue with<Image src={facebookLogo} alt="Facebook Logo" width={25} height={25} className={styles.logoFacebook} /></div>
+            </Button>
+          </>
+        }
+      </Form>
+
+      }
+    </div>
   )
   }
