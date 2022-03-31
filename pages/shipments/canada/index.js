aaa54974ba36577efd5625db9e465ac4b6e23be9
DevNeoLee <devneolee@gmail.com>
2022-03-31 00:19:08 -0700

    Implement Tables responsive in two different ways

--- pages/shipments/canada/index.js
+++ pages/shipments/canada/index.js
@@ -3,6 +3,8 @@ import Image from "next/image";
 import React, { useState } from "react";
 import Link from "next/link";
 
+import { Table } from 'react-bootstrap'
+
 import Layout from "../../../components/layout/Layout";
 import { CloudDownload } from "react-bootstrap-icons";
 
@@ -15,86 +17,84 @@ export default function ShipmentsToCanada() {
         <link rel="icon" href="/favicon.png" />
       </Head>
       <main>
-        <body>
-          <h1>Recent Shipments to Canada</h1>
-          <div id="wrapper">
-            <table>
-              <thead>
-                <tr>
-                  <th>File Number</th>
-                  <th>Trans Number</th>
-                  <th>Reference</th>
-                  <th>Customs Status Date/Time</th>
-                  <th>Status</th>
-                  <th>
-                    <CloudDownload />
+        <h1>Recent Shipments to Canada</h1>
+        <div id="wrapper">
+            <table className="ble">
+              <thead className=" ead">
+                <tr className="rr">
+                  <th className="hh">File Number</th>
+                  <th className="hh">Trans Number</th>
+                  <th className="hh">Reference</th>
+                  <th className="hh">Customs Status Date/Time</th>
+                  <th className="hh">Status</th>
+                  <th className="hh"h>
+                    <CloudDownload size={22}/>
                   </th>
-                  <th>Port of Entry</th>
-                  <th>Carrier</th>
-                  <th>Cargo/PARS Number</th>
-                  <th>View</th>
-                  <th>Client</th>
+                  <th className="hh">Port of Entry</th>
+                  <th className="hh">Carrier</th>
+                  <th className="hh">Cargo/PARS Number</th>
+                  <th className="hh">View</th>
+                  <th className="hh">Client</th>
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td data-label="File Number">1234</td>
-                  <td data-label="Trans Number">77777</td>
-                  <td data-label="Reference">234324343</td>
-                  <td data-label="Customs Status Date/Time">Dec 11/2021</td>
-                  <td data-label="Status">Released</td>
-                  <td data-label="<CloudDownload />">Yes</td>
-                  <td data-label="Port of Entryl">Archie</td>
-                  <td data-label="Carrier">Rainbow Inc.</td>
-                  <td data-label="Cargo/PARS Number">BSC2304902349</td>
-                  <td data-label="View">Yes</td>
-                  <td data-label="Client"> Vancouver City University</td>
-                </tr>
-                <tr>
-                  <td data-label="File Number">1234</td>
-                  <td data-label="Trans Number">77777</td>
-                  <td data-label="Reference">234324343</td>
-                  <td data-label="Customs Status Date/Time">Dec 11/2021</td>
-                  <td data-label="Status">Released</td>
-                  <td data-label="<CloudDownload />">Yes</td>
-                  <td data-label="Port of Entryl">Archie</td>
-                  <td data-label="Carrier">Rainbow Inc.</td>
-                  <td data-label="Cargo/PARS Number">BSC2304902349</td>
-                  <td data-label="View">Yes</td>
-                  <td data-label="Client"> Vancouver City University</td>
-                </tr>
-                <tr>
-                  <td data-label="File Number">5008371</td>
-                  <td data-label="Trans Number">13292963424797</td>
-                  <td data-label="Reference">234324343</td>
-                  <td data-label="Customs Status Date/Time">N/A</td>
-                  <td data-label="Status">Received</td>
-                  <td data-label="<CloudDownload />">Yes</td>
-                  <td data-label="Port of Entryl">PACIFIC HWY/SURREY</td>
-                  <td data-label="Carrier">GRANDO LOGISTICS</td>
-                  <td data-label="Cargo/PARS Number">
+                <tr className="rr">
+                  <td data-label="File Number" className="dd">5008371</td>
+                  <td data-label="Trans Number" className="dd">13292963424797</td>
+                  <td data-label="Reference" className="dd">234324343</td>
+                  <td data-label="Customs Status Date/Time" className="dd">N/A</td>
+                  <td data-label="Status" className="dd">Received</td>
+                  <td data-label="<CloudDownload />" className="dd">Yes</td>
+                  <td data-label="Port of Entryl" className="dd">PACIFIC HWY/SURREY</td>
+                  <td data-label="Carrier" className="dd">GRANDO LOGISTICS</td>
+                  <td data-label="Cargo/PARS Number" className="dd">
                     806SEU21CA220261 EITU9301816
                   </td>
-                  <td data-label="View">Yes</td>
-                  <td data-label="Client">300884 BRITISH COLUMBIA LIMIT</td>
+                  <td data-label="View" className="dd">Yes</td>
+                  <td data-label="Client" className="dd">300884 BRITISH COLUMBIA LIMIT</td>
                 </tr>
-                <tr>
-                  <td data-label="File Number">1234</td>
-                  <td data-label="Trans Number">77777</td>
-                  <td data-label="Reference">234324343</td>
-                  <td data-label="Customs Status Date/Time">Dec 11/2021</td>
-                  <td data-label="Status">Released</td>
-                  <td data-label="<CloudDownload />">Yes</td>
-                  <td data-label="Port of Entryl">Archie</td>
-                  <td data-label="Carrier">Rainbow Inc.</td>
-                  <td data-label="Cargo/PARS Number">BSC2304902349</td>
-                  <td data-label="View">Yes</td>
-                  <td data-label="Client"> Vancouver City University</td>
+                <tr className="rr">
+                  <td data-label="File Number" className="dd">1234</td>
+                  <td data-label="Trans Number" className="dd">77777</td>
+                  <td data-label="Reference" className="dd">234324343</td>
+                  <td data-label="Customs Status Date/Time" className="dd">Dec 11/2021</td>
+                  <td data-label="Status" className="dd">Released</td>
+                  <td data-label="<CloudDownload />" className="dd">Yes</td>
+                  <td data-label="Port of Entryl" className="dd">Archie</td>
+                  <td data-label="Carrier" className="dd">Rainbow Inc.</td>
+                  <td data-label="Cargo/PARS Number" className="dd">BSC2304902349</td>
+                  <td data-label="View" className="dd">Yes</td>
+                  <td data-label="Client" className="dd"> Vancouver City University</td>
+                </tr>
+                <tr className="rr">
+                  <td data-label="File Number" className="dd">1234</td>
+                  <td data-label="Trans Number" className="dd">77777</td>
+                  <td data-label="Reference" className="dd">234324343</td>
+                  <td data-label="Customs Status Date/Time" className="dd">Dec 11/2021</td>
+                  <td data-label="Status" className="dd">Released</td>
+                  <td data-label="<CloudDownload />" className="dd">Yes</td>
+                  <td data-label="Port of Entryl" className="dd">Archie</td>
+                  <td data-label="Carrier" className="dd">Rainbow Inc.</td>
+                  <td data-label="Cargo/PARS Number" className="dd">BSC2304902349</td>
+                  <td data-label="View" className="dd">Yes</td>
+                  <td data-label="Client" className="dd"> Vancouver City University</td>
+                </tr>
+                <tr className="rr">
+                  <td data-label="File Number" className="dd">1234</td>
+                  <td data-label="Trans Number" className="dd">77777</td>
+                  <td data-label="Reference" className="dd">234324343</td>
+                  <td data-label="Customs Status Date/Time" className="dd">Dec 11/2021</td>
+                  <td data-label="Status" className="dd">Released</td>
+                  <td data-label="<CloudDownload />" className="dd">Yes</td>
+                  <td data-label="Port of Entryl" className="dd">Archie</td>
+                  <td data-label="Carrier" className="dd">Rainbow Inc.</td>
+                  <td data-label="Cargo/PARS Number" className="dd">BSC2304902349</td>
+                  <td data-label="View" className="dd">Yes</td>
+                  <td data-label="Client" className="dd"> Vancouver City University</td>
                 </tr>
               </tbody>
             </table>
           </div>
-        </body>
       </main>
     </Layout>
   );
--- pages/shipments/us/index.js
+++ pages/shipments/us/index.js
@@ -3,7 +3,10 @@ import Image from "next/image";
 import React, { useState } from "react";
 import Link from "next/link";
 
+import { Table } from 'react-bootstrap'
+
 import Layout from "../../../components/layout/Layout";
+import { CloudDownload } from "react-bootstrap-icons";
 
 export default function ShipmentsToUS() {
   return (
@@ -14,8 +17,84 @@ export default function ShipmentsToUS() {
         <link rel="icon" href="/favicon.png" />
       </Head>
       <main>
-        <h1>Shipments to US</h1>
-        <div className="table">hello world</div>
+        <h1>Recent Shipments to US</h1>
+        <div id="wrapper" className="table-responsive">
+          <table className="table table-striped table-bordered table-hover">
+            <thead className="thead-dark primary">
+              <tr>
+                <th scope="col">File Number</th>
+                <th scope="col">Trans Number</th>
+                <th scope="col">Reference</th>
+                <th scope="col">Customs Status Date/Time</th>
+                <th scope="col">Status</th>
+                <th scope="col">
+                  <CloudDownload size={22}/>
+                </th>
+                <th scope="col">Port of Entry</th>
+                <th scope="col">Carrier</th>
+                <th scope="col">Cargo/PARS Number</th>
+                <th scope="col">View</th>
+                <th scope="col">Client</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td data-label="File Number">1234</td>
+                <td data-label="Trans Number">77777</td>
+                <td data-label="Reference">234324343</td>
+                <td data-label="Customs Status Date/Time">Dec 11/2021</td>
+                <td data-label="Status">Released</td>
+                <td data-label="<CloudDownload />">Yes</td>
+                <td data-label="Port of Entryl">Archie</td>
+                <td data-label="Carrier">Rainbow Inc.</td>
+                <td data-label="Cargo/PARS Number">BSC2304902349</td>
+                <td data-label="View">Yes</td>
+                <td data-label="Client"> Vancouver City University</td>
+              </tr>
+              <tr>
+                <td data-label="File Number">1234</td>
+                <td data-label="Trans Number">77777</td>
+                <td data-label="Reference">234324343</td>
+                <td data-label="Customs Status Date/Time">Dec 11/2021</td>
+                <td data-label="Status">Released</td>
+                <td data-label="<CloudDownload />">Yes</td>
+                <td data-label="Port of Entryl">Archie</td>
+                <td data-label="Carrier">Rainbow Inc.</td>
+                <td data-label="Cargo/PARS Number">BSC2304902349</td>
+                <td data-label="View">Yes</td>
+                <td data-label="Client"> Vancouver City University</td>
+              </tr>
+              <tr>
+                <td data-label="File Number">5008371</td>
+                <td data-label="Trans Number">13292963424797</td>
+                <td data-label="Reference">234324343</td>
+                <td data-label="Customs Status Date/Time">N/A</td>
+                <td data-label="Status">Received</td>
+                <td data-label="<CloudDownload />">Yes</td>
+                <td data-label="Port of Entryl">PACIFIC HWY/SURREY</td>
+                <td data-label="Carrier">GRANDO LOGISTICS</td>
+                <td data-label="Cargo/PARS Number">
+                  806SEU21CA220261 EITU9301816
+                </td>
+                <td data-label="View">Yes</td>
+                <td data-label="Client">300884 BRITISH COLUMBIA LIMIT</td>
+              </tr>
+              <tr>
+                <td data-label="File Number">1234</td>
+                <td data-label="Trans Number">77777</td>
+                <td data-label="Reference">234324343</td>
+                <td data-label="Customs Status Date/Time">Dec 11/2021</td>
+                <td data-label="Status">Released</td>
+                <td data-label="<CloudDownload />">Yes</td>
+                <td data-label="Port of Entryl">Archie</td>
+                <td data-label="Carrier">Rainbow Inc.</td>
+                <td data-label="Cargo/PARS Number">BSC2304902349</td>
+                <td data-label="View">Yes</td>
+                <td data-label="Client"> Vancouver City University</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </main>
     </Layout>
   );
