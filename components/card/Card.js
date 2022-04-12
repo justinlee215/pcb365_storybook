633a4e4bdf482b03f353d13dc51dd9adedab26ae
justinlee215 <justin.lee@pacificgroup.net>
2022-04-12 13:24:35 -0700

    Delete builtin setup for Storybook

--- components/card/Card.js
+++ components/card/Card.js
@@ -1,13 +1,15 @@
-
 export default function Card() {
   return (
     <div className="card">
       <h2>Submit Your Trade Forms Online</h2>
-      <p>Take control of your complex and confusing trade forms with the care of team of custom professionals</p>
+      <p>
+        Take control of your complex and confusing trade forms with the care of
+        team of custom professionals
+      </p>
       <form action="">
         <input type="select" />
         <button>Start</button>
       </form>
     </div>
-  )
+  );
 }
